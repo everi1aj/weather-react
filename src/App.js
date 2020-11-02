5ee4c77e214a8189a4f84145f41b31b90b43973b
everi1aj <71519711+everi1aj@users.noreply.github.com>
2020-11-02 10:15:19 -0500

    Deleted unnecesarry files and moved open source to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
-import logo from "./logo.svg";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Weather App</h1>
-        <Weather city="Paris" />
-      </header>
+      <h1>Weather App</h1>
+      <Weather city="Paris" />
+      <p>
+        Open-source code by{" "}
+        <a href="https://github.com/everi1aj/weather-react.git">
+          Allison Everitt
+        </a>
+      </p>
     </div>
   );
 }
-
-export default App;
